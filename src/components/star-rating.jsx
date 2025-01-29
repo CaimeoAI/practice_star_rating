08b93530f278aa878f3a8e017ac9e86e2be11662
CaimeoAI <judiellezinke@yahoo.de>
2025-01-29 18:14:36 +0100

    completed adding functionality and styling, with simple useStage use on mouse actions as well as simple color change corresponding to said actions. Added some comments for better clarification of some lines

--- src/components/star-rating.jsx
+++ src/components/star-rating.jsx
@@ -10,15 +10,19 @@ export default function StarRating({starAmount = 10}){
     const [currentHover, setCurrentHover] = useState(0)
 
     function handleClick(getCurrentIndex) {
-        console.log(getCurrentIndex)
+        if (currentRating === getCurrentIndex) {
+            setCurrentRating(0)
+        } else {
+            setCurrentRating(getCurrentIndex)
+        }
     }
 
     function handleMouseEnter(getCurrentIndex) {
-        console.log("mouse entered")
+        setCurrentHover(getCurrentIndex)
     }
 
-    function handleMouseLeave(getCurrentIndex) {
-        console.log("mouse left")
+    function handleMouseLeave() {
+        setCurrentHover(currentRating)
     }
 
     return (
@@ -28,9 +32,11 @@ export default function StarRating({starAmount = 10}){
                 //? ...Array(starAmount) uses the spread operator (...) to convert it into an iterable array. The result is an array of undefined values but with starAmount number of elements.Example Output: [undefined, undefined, undefined, undefined, undefined].
                 //? .map() loops through each element in the array. The function inside .map() has two parameters: _ (underscore): This is a convention used when the first parameter is not needed. Normally, .map((value, index) => {...}) is used, but since value is undefined, we ignore it. index: The current index of the element in the array (0, 1, 2, ...).
                 [...Array(starAmount)].map((_,index) => {
-                    
+                    //? The map function iterates over an array of starAmount length, where index starts at 0 for the first element. The += 1 shifts the index so that the first star is at index 1 instead of 0, the second is 2, and so on. This is useful because star ratings generally don't have a "0th" star; they start from 1 star up to starAmount.
+                    index += 1
                     return <FaStar
                             key={index}
+                            className={index <= (currentHover || currentRating) ? "active" : "inactive"}
                             onClick={() => handleClick(index)}
                             onMouseMove={() => handleMouseEnter(index)}
                             onMouseLeave={() => handleMouseLeave(index)}
