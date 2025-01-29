646307f36e780aa35b4257601bebb58823135f4d
CaimeoAI <judiellezinke@yahoo.de>
2025-01-29 15:41:22 +0100

    added empty function for all events regarding the generated star icons

--- src/components/star-rating.jsx
+++ src/components/star-rating.jsx
@@ -5,10 +5,19 @@ import {FaStar} from 'react-icons/fa'
 //? starAmount as props, if starAmount is not passed down by parent component it defaults to 5
 export default function StarRating({starAmount = 5}){
 
-    const [currentRating, setCurrentRating] = useState()
+    const [currentRating, setCurrentRating] = useState(0)
+    const [currentHover, setCurrentHover] = useState(0)
 
-    function setRating() {
+    function handleClick(getCurrentIndex) {
         
+    }
+
+    function handleMouseEnter(getCurrentIndex) {
+
+    }
+
+    function handleMouseLeave(getCurrentIndex) {
+
     }
 
     return (
@@ -19,7 +28,13 @@ export default function StarRating({starAmount = 5}){
                 //? .map() loops through each element in the array. The function inside .map() has two parameters: _ (underscore): This is a convention used when the first parameter is not needed. Normally, .map((value, index) => {...}) is used, but since value is undefined, we ignore it. index: The current index of the element in the array (0, 1, 2, ...).
                 [...Array(starAmount)].map((_,index) => {
                     
-                    return <FaStar key={index} onClick={() => setRating()} onMouseMove={} onMouseLeave={} size={40}/>
+                    return <FaStar 
+                            key={index} 
+                            onClick={() => handleClick()} 
+                            onMouseMove={() => handleMouseEnter()} 
+                            onMouseLeave={() => handleMouseLeave()} 
+                            size={40}    
+                            />
                 })
             }
         </div>
